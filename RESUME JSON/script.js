568f1e250c4b86e610682423e5510b53f23c2c53
SURYA U <159547373+suryafsdsurya@users.noreply.github.com>
2024-02-24 11:46:33 +0530

    Add files via upload

---
+++ RESUME JSON/script.js
@@ -0,0 +1,32 @@
+const myResume = [
+  {
+    name: "Surya",
+    mobile:  9943882385,
+    email: "suryau816@gmail.com",
+    degree: "B.SC",
+    location: {
+      address: "57/A School Street",
+      "postal code": 606603,
+      village: "Thenimalai",
+      city: "Tiruvannamalai",
+    },
+    education: [
+      {
+        institution: "S.I.A.S.C",
+        dept: "Mathematics",
+        BATCH: 2019,
+        cgpa: 8.4,
+        schoolings: "S.I.G.H.S.S",
+        "sslc percentage": 86,
+        "hsc percentage": 67,
+      },
+    ],
+    skills: [
+      {
+        programming: "Html,java,javascript",
+        certification: "coursera certificate for c programming",
+      },
+    ],
+  },
+];
+console.log(myResume);
